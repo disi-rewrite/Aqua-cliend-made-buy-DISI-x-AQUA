01367deff0f1fa61298a0acdd7f628b982797010
disi-rewrite <migjabib6@gmail.com>
2025-07-22 06:34:08 -0700

    Update AquaClientNewMainScript.lua

--- AquaClientNewMainScript.lua
+++ AquaClientNewMainScript.lua
@@ -1,39 +1,45 @@
-local registry = {
-  ['11630038968'] = 'Bridge Duels',
-  ['6872274481'] = 'Bedwars Match',
-}
+run(function()
+        local InfiniteJump: table = {["Enabled"] = false}
+    InfiniteJump= vape.Categories.Blatant:CreateModule({
+        ["Name"] = "InfiniteJump",
+            ["HoverText"] = "Let's you jump in the air.",
+        ["Function"] = function(callback: boolean): void
+            if callback then
+                local connection: any = inputService["JumpRequest"]:Connect(function()
+                    lplr["Character"]["Humanoid"]:ChangeState("Jumping");
+                end);
+                        InfiniteJump["Connection"] = connection;
+            else
+                        if InfiniteJump["Connection"] then
+                            InfiniteJump["Connection"]:Disconnect();
+                            InfiniteJump["Connection"] = nil;
+                        end;
+            end;
+        end,
+    })
+end)
 
-registry.__index = function(...): boolean return false end
+run(function()
+    local ShaderToggle
 
-while not _G.LunarVape do task.wait() end
-local LunarVape = _G.LunarVape
-if not registry[tostring(LunarVape.Place)] then return end
-local GAME_NAME = tostring(LunarVape.Place)
+    ShaderToggle = vape.Categories.Render:CreateModule({
+        Name = "RealisticShader",
+        Function = function(enabled)
+            local lighting = game:GetService("Lighting")
+            local bloom = lighting:FindFirstChild("Bloom")
+            local colorCorrection = lighting:FindFirstChild("ColorCorrection")
 
-local function downloadFile(path, func)
-    if not isfile(path) and not _G.LunarVapeDeveloper then
-        local suc, res = pcall(function()
-            return game:HttpGet('https://raw.githubusercontent.com/AtTheZenith/LunarVape/'..(isfile('Lunar Vape/Profiles/commit.txt') and readfile('Lunar Vape/Profiles/commit.txt') or 'main')..'/'..(string.gsub(path, 'Lunar Vape/', '')), true)
-        end)
-        if res == '404: Not Found' or res == '' then
-            warn(string.format('Error while downloading file %s: %s', path, res)); return
-        elseif not suc then
-            error(string.format('Error while downloading file %s: %s', path, res)); return
-        end
-        if path:find('.lua') then
-            res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after Lunar Vape updates.\n'..res
-        end
-        writefile(path, res)
-    end
-    return (func or readfile)(path)
-end
-
-local Dir = string.format('Lunar Vape/Extra/Profiles/%s', registry[GAME_NAME])
-if not isfolder(Dir) then makefolder(Dir) end
-
-local Files = loadstring(downloadFile(Dir .. '/Files.lua'))()
-if not isfolder('Lunar Vape/Profiles') then makefolder('Lunar Vape/Profiles') end
-for _, File in Files do
-  if isfile('Lunar Vape/Profiles/' .. File) then continue end
-  writefile('Lunar Vape/Profiles/' .. File, downloadFile(string.format('Lunar Vape/Extra/Profiles/%s/%s', registry[GAME_NAME], File)))
-end
+            if enabled then
+                -- Enable shader effects
+                if bloom then bloom.Enabled = true end
+                if colorCorrection then colorCorrection.Enabled = true end
+            else
+                -- Disable shader effects
+                if bloom then bloom.Enabled = false end
+                if colorCorrection then colorCorrection.Enabled = false end
+            end
+        end,
+        Tooltip = "Toggle realistic shader effects (Bloom + ColorCorrection)"
+    })
+end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()\
